6231be7ac04912237a42ccca1b2f23ad476f98e8
urbanware-org <github@urbanware.org>
2018-04-28 19:04:22 +0200

    Initial upload

---
+++ README.md
@@ -0,0 +1,66 @@
+# ***honeypot-hornet***
+
+**Table of contents**
+*   [Definition](#definition)
+*   [Details](#details)
+*   [Usage](#usage)
+
+----
+
+## Definition
+
+An enhanced version of [honeypot-wasp](https://github.com/urbanware-org/honeypot-wasp) with a fake username and password prompt.
+
+[Top](#honeypot-hornet-)
+
+## Details
+
+Many websites provide administrative access to a management interface for the webmaster, mostly via a sub-directory called `/admin` and also `/login`.
+
+In case your website does not have such a directory (or with a different name), you can create a fake one and use this honeypot to see the access attempts.
+
+[Top](#honeypot-hornet-)
+
+## Usage
+
+### Installation
+
+Installing the honeypot is simple.
+
+1.  Edit `honeypot-hornet.php` and change the name of the log file to something less guessable than `logfile.txt`.
+    ```php
+    $file = 'logfile.txt';
+    ```
+1.  Create an empty text file with that name.
+1.  Rename `honeypot-hornet.php` to `index.php` or `index.html`.
+1.  Create the desired directory where you want to install the honeypot on your web server, e. g. `/admin`.
+1.  Upload the renamed file as well as the empty text file into that directory.
+
+### Function test
+
+<img src="https://github.com/urbanware-org/honeypot-hornet/blob/master/login.png" alt="Fake login prompt" align="right"/>Use your web browser and navigate to the directory on your website which contains the honeypot file. It will show a login prompt asking for the username and password.
+
+The login data can also be given via URL, for example:
+
+```
+https://www.foo.bar/admin?username=admin&password=asdf1234
+```
+
+*   If a user name other than `admin` is given it will return that the user does not exist.
+*   If you try logging in as `admin` it will always return that the password for `admin` is invalid.
+*   If no username and password is given it will simply ask to log in.
+
+The allegedly failed attempts return the code **401** (**Unauthorized**).
+
+After that the attempted login has been logged into the given log file. For example:
+
+```
+[2018-04-28 - 09:42:10] Attempt to log in at 'https://www.foo.bar/admin'
+[2018-04-28 - 09:42:10] Login method: 'POST'
+[2018-04-28 - 09:42:10] Username: 'admin'
+[2018-04-28 - 09:42:10] Password: 'asdf1234'
+[2018-04-28 - 09:42:10] IP address: 192.168.1.2
+[2018-04-28 - 09:42:10] User agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36
+```
+
+[Top](#honeypot-hornet-)
---
+++ honeypot-hornet.php
@@ -0,0 +1,73 @@
+<html>
+  <head>
+    <style>
+      body {
+        font-family: Arial, Helvetica, sans-serif;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="login" style="border-style: solid; margin-left: auto; margin-right: auto; padding: 0px 10px 0px 10px; width: 400px;">
+      <h1>Administration</h1>
+      <?php
+          /******************************************************************************
+           * honeypot-hornet - Enhanced website directory honeypot                      *
+           * Copyright (C) 2018 by Ralf Kilian                                          *
+           * Distributed under the MIT License (https://opensource.org/licenses/MIT)    *
+           *                                                                            *
+           * GitHub: https://github.com/urbanware-org/honeypot-hornet                   *
+           ******************************************************************************/
+        
+          if ($_POST['username'] != "") {
+              $username = $_POST['username'];
+              $password = $_POST['password'];
+              $method = "POST";
+          } else {
+              $username = $_GET['username'];
+              $password = $_GET['password'];
+              $method = "GET";
+          }            
+          
+          if ($username == "") {
+              $subtitle = "Please log in.";
+              $method = "";
+          } elseif ($username == "admin") {
+              $subtitle = "Invalid password for user " . $username . "!";
+              sleep(rand(1, 2));
+              http_response_code(401);    // requires PHP 5.4 or higher
+          } else {
+              $subtitle = "User " . $username . " does not exist!";
+              sleep(rand(1, 2));
+              http_response_code(401);    // requires PHP 5.4 or higher
+          }
+          
+          // Log the login attempt
+          if ($method != "") {
+              $directory = getcwd();
+              $agent = $_SERVER['HTTP_USER_AGENT'];
+              if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                  $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+              } else {
+                  $ip_address = $_SERVER['REMOTE_ADDR'];
+              }
+              
+              $file = 'logfile.txt';
+              $timestamp = date("[Y-m-d - H:i:s]");
+              file_put_contents($file, $timestamp . " Attempt to log in at '" . $directory . "'\n", FILE_APPEND);
+              file_put_contents($file, $timestamp . " Login method: '" . $method . "'\n", FILE_APPEND);
+              file_put_contents($file, $timestamp . " Username: '" . $username . "'\n", FILE_APPEND);
+              file_put_contents($file, $timestamp . " Password: '" . $password . "'\n", FILE_APPEND);
+              file_put_contents($file, $timestamp . " IP address: " . $ip_address . "\n", FILE_APPEND);
+              file_put_contents($file, $timestamp . " User agent: " . $agent . "\n\n", FILE_APPEND);
+          }
+      ?>        
+      <form action="index.php" method="post">
+      <?php echo "<p>" . $subtitle . "</p>"; ?>
+        Username: <br /><input type="text" name="username" style="width: 100%" /><br /><br />
+        Password: <br /><input type="password" name="password" style="width: 100%" /><br /><br />
+        <input type="Submit" value="Login" />
+      </form>
+    </div>
+  </body>
+</html>
+
